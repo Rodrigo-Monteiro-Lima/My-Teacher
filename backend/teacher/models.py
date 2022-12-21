dc153087ceb56b521caad84a6a042523d6e8b758
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-21 12:03:08 -0300

    feat: lesson model

--- backend/teacher/models.py
+++ backend/teacher/models.py
@@ -7,3 +7,10 @@ class Teacher(models.Model):
         max_digits=9, decimal_places=2, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
     image = models.URLField(max_length=255, null=False, blank=False)
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(max_length=255, null=False, blank=False)
+    teacher = models.ForeignKey(
+        to=Teacher, on_delete=models.CASCADE, related_name="classes", null=False, blank=False)
