9b73820bfee2fcf5343392bd3d508938f7f36355
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-21 14:22:12 -0300

    feat: validate name

--- backend/teacher/serializers.py
+++ backend/teacher/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from teacher.models import Teacher, Lesson
+from django.forms import ValidationError
 
 
 class TeacherSerializer(serializers.ModelSerializer):
@@ -11,6 +12,11 @@ class TeacherSerializer(serializers.ModelSerializer):
 class RegistrateClassSerializer(serializers.Serializer):
     email = serializers.EmailField(max_length=255)
     name = serializers.CharField(max_length=100)
+
+    def validate_name(self, value):
+        if len(value) < 3:
+            raise ValidationError("Deve ter pelo menos três caracteres")
+        return value
 
 
 class ClassSerializer(serializers.ModelSerializer):
