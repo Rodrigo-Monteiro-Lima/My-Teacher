b93dc744a3ea42737cf0e03947e928bd7aed5e76
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-21 12:07:42 -0300

    feat: lesson serializer

--- backend/teacher/serializers.py
+++ backend/teacher/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
-from teacher.models import Teacher
+from teacher.models import Teacher, Lesson
 
 
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
         fields = ('id', 'name', 'hourly_pay', 'description', 'image')
+
+
+class RegistrateClassSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=255)
+    name = serializers.CharField(max_length=100)
+
+
+class ClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
