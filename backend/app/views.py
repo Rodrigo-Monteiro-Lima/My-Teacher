160441fa47fc81bcb8d00f433e4ca52fc73a24fe
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-21 11:18:51 -0300

    feat: teacher

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
-def home(request):
-    return render(request, 'app/home.html')
+class HomeApiView(APIView):
+    def get(self, request, format=None):
+        return Response({"nome": "Rodrigo Lima", "idage": 22}, status=200)
