8d70203fac91e21bfb75c5fe10cb9dca89bd4e89
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-21 11:50:11 -0300

    feat: teachers view

--- backend/myteacher/urls.py
+++ backend/myteacher/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app.views import HomeApiView
+from teacher.views import TeacherAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeApiView.as_view())
+    path('', HomeApiView.as_view()),
+    path('teachers/', TeacherAPIView.as_view()),
 ]
--- backend/teacher/views.py
+++ backend/teacher/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.views import Response
+from teacher.models import Teacher
+from teacher.serializers import TeacherSerializer
+from rest_framework.status import HTTP_200_OK
 
-# Create your views here.
+
+class TeacherAPIView(APIView):
+    def get(self, request, format=None):
+        teachers = Teacher.objects.all()
+        serializer = TeacherSerializer(teachers, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
